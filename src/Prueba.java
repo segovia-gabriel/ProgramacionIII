b9ccd16e9deefa1547f6fb8cf397063214d8bf4c
segovia-gabriel <segodate@gmail.com>
2025-06-05 20:50:43 -0300

    Modifique algo

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba {
     public static void main(String[] args) {
-        System.out.println("Esta es una prueba");
+        System.out.println("Esta es una prueba 123");
     }
 }
