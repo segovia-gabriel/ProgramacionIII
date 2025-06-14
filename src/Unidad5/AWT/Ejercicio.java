97dde5a0b4ade6e6a6cf035aaf5632f72be3aa3c
segovia-gabriel <segodate@gmail.com>
2025-06-14 03:21:31 -0300

    Modificaciones

--- src/Unidad5/AWT/Ejercicio.java
+++
@@ -1,5 +0,0 @@
-package Unidad5.AWT;
-
-public class Ejercicio {
-
-}
---
+++ src/Unidad5/AWT/EjercicioVentanaLogin.java
@@ -0,0 +1,11 @@
+package Unidad5.AWT;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.awt.*;
+
+public class EjercicioVentanaLogin extends Frame {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
